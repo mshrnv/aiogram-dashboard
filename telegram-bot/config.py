e8b07f0da512ecb00a1450745adf880008b14e60
shmd001 <cool.shoronov19@gmail.com>
2022-11-19 00:41:31 +0300

    Created Telegram bot

---
+++ telegram-bot/config.py
@@ -0,0 +1,3 @@
+"""Config Module"""
+
+BOT_TOKEN = "5941002986:AAELg3lWzQD9tpGTf8faNOvdDgNTBj7goSw"
---
+++ telegram-bot/main.py
@@ -0,0 +1,36 @@
+"""Telegram Bot Main Module"""
+
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from config import BOT_TOKEN
+
+# Enabling logging
+logging.basicConfig(level=logging.INFO)
+
+# Bot object
+bot = Bot(token=BOT_TOKEN)
+
+# Dispatcher
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def cmd_start(message: types.Message):
+    """Responds to the start command
+
+    Args:
+        message (Message): "/start" message
+    """
+    await message.answer("Hello!)")
+
+
+async def main():
+    """Includes routers and launching the process of polling new updates"""
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
