9d25595e13ee4700a47c914c5a537dcc48922fe3
shmd001 <cool.shoronov19@gmail.com>
2022-11-19 14:59:48 +0300

    Server index refactored

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const usersRouter = require('./routes/users')
 
-const PORT = 8000
+const port = 8000
 const app = express()
 
-app.listen(PORT, () => {
-    console.log("Server has been started...")
-})
+app.use(usersRouter)
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://mongo:27017/telegram-bot')
+        app.listen(port, () => {
+            console.log("Server has been started...")
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
