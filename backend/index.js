4be9afd8b90ef72591c035577af449d37afcef51
shmd001 <cool.shoronov19@gmail.com>
2022-11-19 15:09:12 +0300

    Backend refactoring

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,10 @@ const usersRouter = require('./routes/users')
 const port = 8000
 const app = express()
 
+// Including users router
 app.use(usersRouter)
 
+// Connects to MongoDB and starts ther server
 const start = async () => {
     try {
         await mongoose.connect('mongodb://mongo:27017/telegram-bot')
